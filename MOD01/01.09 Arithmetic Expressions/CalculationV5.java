503b00c91d7745cdef11d49b796025d9abdd09ed
bagcheap <spare_brain@yahoo.com>
2024-06-15 16:08:13 -0400

    After completing 01.09 exercise

--- MOD01/01.09 Arithmetic Expressions/CalculationV5.java
+++ MOD01/01.09 Arithmetic Expressions/CalculationV5.java
@@ -1,44 +1,41 @@
-public class CalculationV3 {
+public class CalculationV5 {
     public static void main(String[] args) {
-        // Addition
+        // Declare integer variables
+
         // Declare integer variables
         int iNum1 = 25;
         int iNum2 = 9;
         int iNum3 = 11;
+        int iNum4 = 10;
 
-        // Addition
-        System.out.println("Addition");
-        System.out.println(iNum1 + " + " + iNum2 + " = " + (iNum1 + iNum2));
-        System.out.println(43.21 + " + " + 3.14 + " = " + (43.21 + 3.14));
-        System.out.println();
+        // Declare double variables
+        double dNum1 = 43.21;
+        double dNum2 = 3.14;
+        double dNum3 = 10.0;
 
         // Subtraction
         System.out.println("Subtraction");
         System.out.println(iNum3 + " - " + iNum2 + " - " + iNum1 + " = " + (iNum3 - iNum2 - iNum1));
-        System.out.println("3.14 - 10.0 = " + (3.14 - 10.0));
+        System.out.println(dNum2 + " - " + dNum3 + " = " + (dNum2 - dNum3));
         System.out.println();
 
         // Multiplication
         System.out.println("Multiplication");
         System.out.println(iNum1 + " * " + iNum2 + " = " + (iNum1 * iNum2));
-        System.out.println("3.14 * 10.0 * 10.0 = " + (3.14 * 10.0 * 10.0));
+        System.out.println(dNum2 + " * " + dNum3 + " * " + dNum3 + " = " + (dNum2 * dNum3 * dNum3));
         System.out.println();
 
         // Division
         System.out.println("Division");
         System.out.println(iNum2 + " / " + iNum1 + " = " + (iNum2 / iNum1));
-        System.out.println("43.21 / 10.0 = " + (43.21 / 10.0));
+        System.out.println(dNum1 + " / " + dNum3 + " = " + (dNum1 / dNum3));
         System.out.println();
 
         // Modulus operator
         System.out.println("Modulus");
         System.out.println(iNum3 + " % " + iNum2 + " = " + (iNum3 % iNum2));
-        System.out.println("10.0 % 3.14 = " + (10.0 % 3.14));
+        System.out.println(dNum3 + " % " + dNum2 + " = " + (dNum3 % dNum2));
         System.out.println();
-
-        // 1.08 Additional int Equations
-
-        // 1.09 Additional double Equations
 
     }
 }
