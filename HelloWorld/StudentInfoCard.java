554acdac197e91dc83c2d623428d3b812e184594
angus <spare_brain@yahoo.com>
2024-06-15 10:30:25 -0400

    Completed code

--- HelloWorld/StudentInfoCard.java
+++ HelloWorld/StudentInfoCard.java
@@ -13,29 +13,34 @@ public class StudentInfoCard {
         System.out.println("*                    Student Information                       *");
         System.out.println("*                                                              *");
         System.out.println("****************************************************************\n\n");
-        System.out.print(
-                "Name:" + "Rhea Bagchee" + "\t" +
-                        "Birthday: " + "March 15, 2008" + "\n" +
-                        "Go-by-Name: " + "Rhea" + "\t" + "Age: " + "15 years" + "\t" + "Grade: 11 \n\n");
+        
+        System.out.print("Name: " + "Rhea Bagchee" + "\t");
+        System.out.println("Birthday: " + "March 15, 2008");
+        System.out.print("Go-by-Name: " + "Rhea" + "\t");
+        System.out.print("Age: " + "15 years" + "\t");
+        System.out.println("Grade: 11 \n\n");
 
-        System.out.println("School: " + "Seminole High School\t" + "City: Sanford, FL\n\n");
+        System.out.print("School: " + "Seminole High School\t");
+        System.out.println("City: Sanford, FL\n\n");
 
-        System.out.print("Guardian Contact Information:");
-        System.out.print(
-                "Name: " + "Rupa Bagchee\t" + "Ph: (407) 462-2585" + "\n" +
-                        "Home Phone: " + "(407)-462-2587\t" + "Availability: " + "M-F 09:00-17:00\n" +
-                        "Cell Phone: " + "(407)-462-2587" + "\t" +
-                        "Availability: " + "M-F 09:00-17:00" + "\n" +
-                        "Email: " + "rbagchee@gmail.com\n\n");
+        System.out.println("Guardian Contact Information:");
+        System.out.println("Name: " + "Rupa Bagchee\t");
 
-        System.out.println("Math Course: " + "AP Algebra 1");
+        System.out.print("Home Phone: " + "(407)-462-2587\t");
+        System.out.println("Availability: " + "M-F 09:00-17:00");
+        System.out.print("Cell Phone: " + "(407)-462-2587" + "\t");
+        System.out.println("Availability: " + "M-F 09:00-17:00");
+        System.out.println("Email: " + "rbagchee@gmail.com\n");
+
+        System.out.println("Highest Math Course Taken: " + "AP Algebra 1\n");
+
         System.out.println("\nComputer Experience:");
-        System.out.println("1. Programming in Scratch-MIT (Gold Medal Winner in Science Olympiad Code Challenge)\n" +
-                "2. C programming with Vex Robotics\n" +
-                "3. Python Programming (rudimentary)");
+        System.out.println("1. Programming in Scratch-MIT (Gold Medal Winner in Science Olympiad Code Challenge)");
+        System.out.println("2. C programming with Vex Robotics");
+        System.out.println("3. Python Programming (rudimentary)\n");
         System.out.print("Reason for taking this course: \n");
-        System.out.println("Part of the AP Curriculum and interest in applied AI/ML in Medicine\n");
-
+        System.out.println("Part of the AP Curriculum and interest in applied AI/ML in Medicine. \nThis is currently a major area of focus. With the advent of sources like AlphaFold3, \nprovides an exciting araay of possibilities!");
+        System.out.println("\n******************End of Student Info Card*********************\n\n");
     }
 
 }
