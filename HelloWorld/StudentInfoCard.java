3a27a275d43953632348495d3d51da6b88b4c642
projectscienceclub <neeltulips@yahoo.com>
2024-06-15 08:57:11 -0400

    Initial Commit

---
+++ HelloWorld/StudentInfoCard.java
@@ -0,0 +1,41 @@
+/**
+ * Write a description of class StudentInfoCard here.
+ *
+ * @author Rhea Bagchee
+ * @version 2024-06-01
+ * @description This file prints out the Student Information
+ */
+public class StudentInfoCard {
+    public static void main(String[] args) {
+        System.out.println("\n\n");
+        System.out.println("****************************************************************");
+        System.out.println("*                                                              *");
+        System.out.println("*                    Student Information                       *");
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************\n\n");
+        System.out.print(
+                "Name:" + "Rhea Bagchee" + "\t" +
+                        "Birthday: " + "March 15, 2008" + "\n" +
+                        "Go-by-Name: " + "Rhea" + "\t" + "Age: " + "15 years" + "\t" + "Grade: 11 \n\n");
+
+        System.out.println("School: " + "Seminole High School\t" + "City: Sanford, FL\n\n");
+
+        System.out.print("Guardian Contact Information:");
+        System.out.print(
+                "Name: " + "Rupa Bagchee\t" + "Ph: (407) 462-2585" + "\n" +
+                        "Home Phone: " + "(407)-462-2587\t" + "Availability: " + "M-F 09:00-17:00\n" +
+                        "Cell Phone: " + "(407)-462-2587" + "\t" +
+                        "Availability: " + "M-F 09:00-17:00" + "\n" +
+                        "Email: " + "rbagchee@gmail.com\n\n");
+
+        System.out.println("Math Course: " + "AP Algebra 1");
+        System.out.println("\nComputer Experience:");
+        System.out.println("1. Programming in Scratch-MIT (Gold Medal Winner in Science Olympiad Code Challenge)\n" +
+                "2. C programming with Vex Robotics\n" +
+                "3. Python Programming (rudimentary)");
+        System.out.print("Reason for taking this course: \n");
+        System.out.println("Part of the AP Curriculum and interest in applied AI/ML in Medicine\n");
+
+    }
+
+}
