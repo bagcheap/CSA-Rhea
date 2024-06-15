80c12aa8aae1181db435f02e154da03ab75e342d
bagcheap <spare_brain@yahoo.com>
2024-06-15 14:14:04 -0400

    Fixing CalculationsV1 and V2 so they match up with instructions.

---
+++ MOD01/01.08 Arithmetic Expressions in the Mod01/CalculationV4.java
@@ -0,0 +1,23 @@
+public class CalculationV4 {
+    public static void main(String[] args) {
+        // Declare integer variables
+        int iNum1 = 25;
+        int iNum2 = 9;
+        // Addition
+        System.out.println("Addition");
+        System.out.println(iNum1 + " + " + iNum2 + " = " + (iNum1 + iNum2));
+        System.out.println(43.21 + " + " + 3.14 + " = " + (43.21 + 3.14));
+        System.out.println();
+
+        // Subtraction
+        // Declare integer variables
+        iNum1 = 25;
+        iNum2 = 9;
+        // Subtraction
+        System.out.println("Subtraction");
+        System.out.println(iNum1 + " - " + iNum2 + " = " + (iNum1 - iNum2));
+        System.out.println(43.21 + " - " + 3.14 + " = " + (43.21 - 3.14));
+        System.out.println();
+
+    }
+}
