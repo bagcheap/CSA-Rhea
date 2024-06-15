d1ff6ea0fc3e355a48d57c7bcc6bbd42ed27879b
bagcheap <spare_brain@yahoo.com>
2024-06-15 16:31:08 -0400

    After completing assignment 01.09

--- MOD01/01.09 Assignment/CalculationV6.java
+++ MOD01/01.09 Assignment/CalculationV6.java
@@ -1,4 +1,4 @@
-public class CalculationV5 {
+public class CalculationV6 {
     public static void main(String[] args) {
         // Declare integer variables
 
@@ -6,11 +6,13 @@ public class CalculationV5 {
         int iNum1 = 25;
         int iNum2 = 9;
         int iNum3 = 11;
+        int iNumNew = 24;
 
         // Declare double variables
         double dNum1 = 43.21;
         double dNum2 = 3.14;
         double dNum3 = 10.0;
+        double dNumNew = 12.5;
 
         // Subtraction
         System.out.println("Subtraction");
@@ -34,6 +36,22 @@ public class CalculationV5 {
         System.out.println("Modulus");
         System.out.println(iNum3 + " % " + iNum2 + " = " + (iNum3 % iNum2));
         System.out.println(dNum3 + " % " + dNum2 + " = " + (dNum3 % dNum2));
+        System.out.println();
+
+        // New mixed type and mixed operator example 1
+        System.out.println("Mixed type and mixed operator");
+        System.out
+                .println(iNum3 + " + " + iNum2 + dNumNew + "/" + iNumNew + " = " + (iNum3 + iNum2 + dNumNew / iNumNew));
+        System.out.println(iNum1 + " % " + iNum2 + " / " + dNumNew + " * " + "(" + dNum3 + "*" + iNumNew + ")" + " = "
+                + (iNum1 % iNum2 / dNumNew * (dNum3 * iNumNew)));
+        System.out.println();
+
+        // New mixed type and mixed operator example 2
+        System.out.println("Mixed type and mixed operator");
+        System.out.println(
+                iNum1 + " - " + iNum2 + " * " + dNumNew + " + " + dNum3 + " = " + (iNum1 - iNum2 * dNumNew + dNum3));
+        System.out.println(dNum3 + " / " + iNum3 + " + " + iNumNew + " - " + "(" + dNumNew + " * " + iNum2 + ")" + " = "
+                + (dNum3 / iNum3 + iNumNew - (dNumNew * iNum2)));
         System.out.println();
 
     }
