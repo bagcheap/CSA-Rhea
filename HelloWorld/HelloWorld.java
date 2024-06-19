057aa55d750ec109e6d6944b06b30e05283d8074
bagcheap <spare_brain@yahoo.com>
2024-06-18 22:36:34 -0400

    After adding sec 1.11

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        double average = (4 + 5 + 3 + 3 + 4) / 5d;
+        System.out.println("The GPA is: " + average);
         System.out.println("Hello World!!!");
     }
 }
